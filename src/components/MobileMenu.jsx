1b26bef1b00f26db257e48e94ffb1a07dbd35eca
Alirahmon777 <alirahmonuraliev007@gmail.com>
2023-11-04 18:12:54 +0500

    fixed-mobile-nav

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -3,13 +3,15 @@ import { footer, header } from '../static/data';
 import { Link, NavLink } from 'react-router-dom';
 import { novaIcon } from 'assets';
 import { scrollToComponent } from '../utils/scrollToComponents';
+import { useTranslation } from 'react-i18next';
 
 const MobileMenu = ({ open, setOpen }) => {
+  const { t, i18n } = useTranslation();
   return (
     <div className={`mobile-menu md:hidden ${open ? 'open' : ''}`}>
       <div className='mobile-menu__wrap text-center'>
         <ul className='mobile-menu__list text-center'>
-          {header.nav.map(({ component_id, name, child }, i) => (
+          {header.nav.map(({ component_id, label, child }, i) => (
             <React.Fragment key={i}>
               {!child && (
                 <li key={i} className='mb-[1.3rem]' onClick={() => setOpen(false)}>
@@ -18,20 +20,20 @@ const MobileMenu = ({ open, setOpen }) => {
                     className='header__nav__link uppercase'
                     onClick={() => scrollToComponent(component_id.substring(1))}
                   >
-                    {name}
+                    {t(label, { ns: 'layout' })}
                   </NavLink>{' '}
                 </li>
               )}
 
               {child &&
-                child.map(({ component_id, name }, i) => (
+                child.map(({ component_id, label }, i) => (
                   <li className='mb-[1.3rem]' key={i} onClick={() => setOpen(false)}>
                     <NavLink
                       to={component_id}
                       className='header__nav__link uppercase'
                       onClick={() => scrollToComponent(component_id.substring(1))}
                     >
-                      {name}
+                      {t(label, { ns: 'layout' })}
                     </NavLink>
                   </li>
                 ))}
@@ -52,7 +54,7 @@ const MobileMenu = ({ open, setOpen }) => {
         </div>
         <div className='header-tel mt-5'>
           <button className='bg-[var(--brown)] border-[0.1rem] text-white border-white px-[20px] py-1 text-[1rem]'>
-            Ariza qoldiring
+            {t('header_btn', { ns: 'layout' })}
           </button>
         </div>
         <div className='mobile-menu__nova flex justify-center items-center mt-4'>
